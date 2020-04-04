4933f077b184990f905537f3d067f3e8d45a00ef
Nikhil R <nikhil.raut94@gmail.com>
2020-04-05 01:59:51 +0530

    save

--- mysite/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from myapp.views import *
-
-urlpatterns = [
-    path('test', test_view),
-    path('minidatasheet/<slug:ic_name>', minidatasheet_view),
-]
